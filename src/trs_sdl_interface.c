0dafd5004df2f4b7e3200998379ca63d21f07e1c
Jens Guenther <dbotw@gmx.net>
2019-02-24 10:47:47 +0100

    Transpose keys for window scaling

--- src/trs_sdl_interface.c
+++ src/trs_sdl_interface.c
@@ -2055,7 +2055,7 @@ void trs_get_event(int wait)
           }
           break;
         case SDLK_PLUS:
-        case SDLK_PAGEUP:
+        case SDLK_PAGEDOWN:
           if (!fullscreen) {
             scale_x++;
             if (scale_x > MAX_SCALE)
@@ -2068,7 +2068,7 @@ void trs_get_event(int wait)
           }
           break;
         case SDLK_MINUS:
-        case SDLK_PAGEDOWN:
+        case SDLK_PAGEUP:
           if (!fullscreen) {
             scale_x--;
             if (scale_x < 1)
