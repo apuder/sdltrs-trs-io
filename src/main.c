1ba8140d5a2fcd3c8b34cce264dcbec5ed5497de
Jens Guenther <dbotw@gmx.net>
2020-02-26 20:35:54 +0100

    Set environment variable without SDL

--- src/main.c
+++ src/main.c
@@ -181,7 +181,7 @@ int SDLmain(int argc, char *argv[])
     fatal("failed to initialize SDL: %s", SDL_GetError());
 
 #ifndef SDL2
-  SDL_putenv("SDL_VIDEO_CENTERED=center");
+  putenv("SDL_VIDEO_CENTERED=center");
 
   SDL_EnableKeyRepeat(SDL_DEFAULT_REPEAT_DELAY, SDL_DEFAULT_REPEAT_INTERVAL);
   /* Enable Unicode key translations */
