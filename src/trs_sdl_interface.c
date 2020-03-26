bef155cc0c304ae424cd9f27e036b2e3d3d74205
Jens Guenther <dbotw@gmx.net>
2020-03-26 16:32:33 +0100

    Reduce size of screen copy buffer

--- src/trs_sdl_interface.c
+++ src/trs_sdl_interface.c
@@ -1664,7 +1664,7 @@ void trs_sdl_cleanup(void)
 #if defined(SDL2) || !defined(NOX)
 static char *trs_get_copy_data()
 {
-  static char copy_data[2500];
+  static char copy_data[1920]; /* =80*24 */
   char data;
   char *curr_data = copy_data;
   char *screen_ptr;
