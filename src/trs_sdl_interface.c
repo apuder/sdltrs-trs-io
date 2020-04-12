be6d0ebcd495f95e8b7033dd6da9bbaf8d948c64
Jens Guenther <dbotw@gmx.net>
2020-04-13 00:41:18 +0200

    Prevent crash in SDL cleanup

--- src/trs_sdl_interface.c
+++ src/trs_sdl_interface.c
@@ -1682,14 +1682,18 @@ void trs_sdl_cleanup(void)
   unsigned int i, ch;
 
   /* SDL cleanup */
-  for (i = 0; i < 6; i++)
+  for (i = 0; i < 6; i++) {
     for (ch = 0; ch < MAXCHARS; ch++) {
-      free(trs_char[i][ch]->pixels);
-      SDL_FreeSurface(trs_char[i][ch]);
+      if (trs_char[i][ch]) {
+        free(trs_char[i][ch]->pixels);
+        SDL_FreeSurface(trs_char[i][ch]);
+      }
     }
+  }
   for (i = 0; i < 3; i++)
     for (ch = 0; ch < 64; ch++)
       SDL_FreeSurface(trs_box[i][ch]);
+
   SDL_FreeSurface(image);
 #ifdef SDL2
   SDL_DestroyWindow(window);
