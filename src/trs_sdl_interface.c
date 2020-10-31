2527c9bf941f8ca15567fec54dc321a5bede680a
Jens Guenther <dbotw@gmx.net>
2020-10-31 12:48:31 +0100

    Update licenses in source code
    - The existing MIT license is retained for source code files that were
      taken over from xtrs, even if these have been modified.
    - Only files specific to SDLTRS are under the BSD-2 license.

--- src/trs_sdl_interface.c
+++ src/trs_sdl_interface.c
@@ -1,10 +1,14 @@
-/* SDLTRS version Copyright (c): 2006, Mark Grebe */
-
-/* Redistribution and use in source and binary forms, with or without
+/*
+ * Copyright (C) 2006-2011, Mark Grebe
+ * Copyright (C) 2018-2020, Jens Guenther
+ *
+ * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
+ *
  * 1. Redistributions of source code must retain the above copyright
  *    notice, this list of conditions and the following disclaimer.
+ *
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
@@ -20,27 +24,7 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
-*/
-
-/*
- * Copyright (C) 1992 Clarendon Hill Software.
- *
- * Permission is granted to any individual or institution to use, copy,
- * or redistribute this software, provided this copyright notice is retained.
- *
- * This software is provided "as is" without any expressed or implied
- * warranty.  If this software brings on any sort of damage -- physical,
- * monetary, emotional, or brain -- too bad.  You've got no one to blame
- * but yourself.
- *
- * The software may be modified for your own purposes, but modified versions
- * must retain this notice.
  */
-
-/*
-   Modified by Mark Grebe, 2006
-   Last modified on Wed May 07 09:56:00 MST 2006 by markgrebe
-*/
 
 /*#define MOUSEDEBUG 1*/
 /*#define XDEBUG 1*/
--- src/trs_state_save.c
+++ src/trs_state_save.c
@@ -1,10 +1,14 @@
-/* Copyright (c): 2006, Mark Grebe */
-
-/* Redistribution and use in source and binary forms, with or without
+/*
+ * Copyright (C) 2006-2011, Mark Grebe
+ * Copyright (C) 2018-2020, Jens Guenther
+ *
+ * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
+ *
  * 1. Redistributions of source code must retain the above copyright
  *    notice, this list of conditions and the following disclaimer.
+ *
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
@@ -20,12 +24,7 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
-*/
-
-/*
-   Modified by Mark Grebe, 2006
-   Last modified on Wed May 07 09:12:00 MST 2006 by markgrebe
-*/
+ */
 
 #include <errno.h>
 #include <stdio.h>
